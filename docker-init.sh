382fd04cc060d5a112875f10592ebd613c9b6803
Tkc <loosefingers@gmail.com>
2017-02-28 05:29:13 +0900

    Update docker sh

---
+++ docker-init.sh
@@ -0,0 +1,27 @@
+# env
+export DOCKER_CERT_PATH=~/.docker/machine/machines/default/
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_MACHINE_NAME="default"
+
+# docker-machine
+docker-machine regenerate-certs default
+docker-machine start default
+eval "$(docker-machine env default)"
+
+# docker-compose
+docker rm -f $(docker ps -a -q)
+docker-compose rm --all
+docker-compose up -d
+
+# open web
+function ip() {
+    docker-machine ip
+}
+
+dockerIP=`ip`
+open 'http://'$dockerIP':8888'
+echo $dockerIP':8888'
+
+# exec image
+docker exec -ti tensor1 bash
